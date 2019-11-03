a1dbdf5bc1036154e1c64f35606d602495cf74b6
Nina <ilnarnika@gmail.com>
2019-11-03 20:49:09 +1100

    Space and campaign linkage methods.

--- app/controllers/space_selector_controller.rb
+++ app/controllers/space_selector_controller.rb
@@ -9,26 +9,58 @@ class SpaceSelectorController < ApplicationController
     def index
         @spaces = Space.suitable_for_campaign(
             @campaign.size, @campaign.product_ids)
+        @linked_spaces = Set.new
+        @campaign.spaces.each do |space|
+            @linked_spaces << space.id
+        end
     end
 
-    def add
-
+    def link
+        @spaces = Space.suitable_for_campaign(
+            @campaign.size, @campaign.product_ids)
+        respond_to do |format|
+            space_id = get_space_id.to_i
+            space = @spaces.find { |s| s.id == space_id }
+          if space == nil
+              format.html { redirect_to space_selector, alert: 'Space is not found.' }
+          elsif @campaign.spaces.find { |s| s.id == space_id }
+              format.html { redirect_to space_selector_url, alert: 'Space is already linked.' }
+          else
+              @campaign.spaces << space
+              format.html { redirect_to space_selector_url, notice: 'Space is added.' }
+          end
+        end
     end
 
-    def destroy
-
+    def unlink
+        @spaces = Space.suitable_for_campaign(
+            @campaign.size, @campaign.product_ids)
+        respond_to do |format|
+            space_id = get_space_id.to_i
+            space = @campaign.spaces.find_by_id(space_id)
+            if space == nil
+                format.html { redirect_to space_selector_url, alert: 'Space is not found.' }
+            else
+                @campaign.spaces.delete(space)
+                format.html { redirect_to space_selector_url, notice: 'Space is removed.' }
+            end
+        end
     end
 
     def search
-
     end
 
-private
+    private
 
     def set_user_campaign
-      @campaign = current_user.campaigns.find_by_id(params[:id])
-      if @campaign == nil
-        redirect_to campaign_path
-      end
+        @campaign = current_user.campaigns.find_by_id(params[:id])
+        if @campaign == nil
+            redirect_to campaign_path
+        end
     end
+
+    def get_space_id
+        params.require(:space_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "/", to: "pages#home", as: "root"
   get "/space_selector/campaigns/:id", to: "space_selector#index", as: "space_selector"
+  post "/space_selector/campaigns/:id", to: "space_selector#link"
+  delete "/space_selector/campaigns/:id", to: "space_selector#unlink"
 end
